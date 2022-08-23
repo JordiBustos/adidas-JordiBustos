4aad17be7f3b5d215874c56fe5aabc9fddc855a3
Jordi <jordibustos193771@alumnos.info.unlp.edu.ar>
2022-08-22 22:39:56 -0300

    Format document

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -8,13 +8,13 @@ const ItemCount = ({ stock, initial }) => {
 		<ItemCountContainer>
 			<ItemCountButtonDecrease
 				onClick={() => {
-                    if (ammount === `${stock} (max)`) {
-                        setAmmount(stock-1);
-                    } else {
-                        if (ammount - 1 >= 1) {
-                            setAmmount(ammount - 1);
-                        }
-                    }
+					if (ammount === `${stock} (max)`) {
+						setAmmount(stock - 1);
+					} else {
+						if (ammount - 1 >= 1) {
+							setAmmount(ammount - 1);
+						}
+					}
 				}}
 			>
 				-
@@ -22,76 +22,75 @@ const ItemCount = ({ stock, initial }) => {
 
 			<ItemCountValue>{ammount}</ItemCountValue>
 
-            <ItemCountButton
+			<ItemCountButton
 				onClick={() => {
 					if (ammount + 1 < stock) {
 						setAmmount(ammount + 1);
 					} else {
-                        if (ammount + 1 === stock) {
-                            setAmmount(`${ammount + 1} (max)`);
-                        }
-                    }
+						if (ammount + 1 === stock) {
+							setAmmount(`${ammount + 1} (max)`);
+						}
+					}
 				}}
 			>
 				+
 			</ItemCountButton>
-            <ItemButtonAddCart>Add to cart</ItemButtonAddCart>
+			<ItemButtonAddCart>Add to cart</ItemButtonAddCart>
 		</ItemCountContainer>
 	);
 };
 
 const ItemCountContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-flow: row wrap;
-    width: 15%;
-    padding: 0.5rem;
-    border-radius: 0.5rem;
-    background-color: #f5f5f5;
-    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
-    margin-top:5%;
-    margin-left:10%;
-
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	flex-flow: row wrap;
+	width: 15%;
+	padding: 0.5rem;
+	border-radius: 0.5rem;
+	background-color: #f5f5f5;
+	box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+	margin-top: 5%;
+	margin-left: 10%;
 `;
 const ItemCountButton = styled.button`
-    background-color: black;
-    border:none;
-    color: white;
-    font-size:1.2rem;
-    padding: .3rem .3rem;
-    flex-basis:10%;
+	background-color: black;
+	border: none;
+	color: white;
+	font-size: 1.2rem;
+	padding: 0.3rem 0.3rem;
+	flex-basis: 10%;
 
-    &:hover {
-        color: #7c7c7c;
-    }
+	&:hover {
+		color: #7c7c7c;
+	}
 `;
 const ItemCountValue = styled.span`
-    font-size:1.2rem;
+	font-size: 1.2rem;
 `;
 const ItemCountButtonDecrease = styled.button`
-    background-color: black;
-    border:none;
-    color: white;
-    font-size:1.2rem;
-    padding: .3rem .3rem;
-    flex-basis:10%;
-    &:hover {
-        color: #7c7c7c;
-    }
+	background-color: black;
+	border: none;
+	color: white;
+	font-size: 1.2rem;
+	padding: 0.3rem 0.3rem;
+	flex-basis: 10%;
+	&:hover {
+		color: #7c7c7c;
+	}
 `;
 const ItemButtonAddCart = styled.button`
-    background-color: black;
-    border:none;
-    color: white;
-    font-size:1.2rem;
-    padding: .3rem .3rem;
-    flex-basis:100%;
-    margin-top:1rem;
+	background-color: black;
+	border: none;
+	color: white;
+	font-size: 1.2rem;
+	padding: 0.3rem 0.3rem;
+	flex-basis: 100%;
+	margin-top: 1rem;
 
-    &:hover {
-        color: #7c7c7c;
-    }
+	&:hover {
+		color: #7c7c7c;
+	}
 `;
 
 export default ItemCount;
